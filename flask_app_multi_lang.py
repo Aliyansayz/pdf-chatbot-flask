118348be4cb42e2ec77a575be19723da75bcfc93
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2023-12-28 19:13:59 +0500

    Update flask_app_multi_lang.py

--- flask_app_multi_lang.py
+++ flask_app_multi_lang.py
@@ -10,8 +10,8 @@ def home():
     if request.method == 'POST':
         if 'send' in request.form:
             message = request.form.get('message')
-            messages.append({'text': message, 'sender': 'user'})
-            messages.append({'text': f'Received your message: "{message}"', 'sender': 'guide'})
+            # messages.append({'text': message, 'sender': 'user'}) 
+            messages.append({'text': f'Received your message: {message}' , 'sender': f"{message}" } )
         
         elif 'reset' in request.form:
             messages = []
@@ -24,10 +24,70 @@ def home():
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
             <script src="./static/script.js"></script>
             <style>
+
+                body {
+                    font-family: 'Roboto', sans-serif;
+                }
+
+                .container {
+                    width: 100%;
+                }
+
+                .row {
+                    margin: 0;
+                }
+
+                .display-4 {
+                    font-weight: 700; /* Bold */
+                }
+
+                .lead {
+                    font-weight: 400; /* Regular */
+                }
+
+
+                .container {
+                    width: 100%;
+                }
+
+                .row {
+                    margin: 0;
+                }
+
+                .chat-element {
+                    height: 200px;
+                    overflow-y: auto;
+                    padding: 10px;
+                    display: flex;
+                    flex-direction: column;
+                }
+
+                .message-box {
+                    border-radius: 10px;
+                    margin-bottom: 10px;
+                    padding: 10px;
+                    color: white;
+                    word-wrap: break-word; /* This will prevent long text from overflowing */
+                    display: inline-block; /* This will make the width of the box adjust to the text */
+                }
+
+                .sender {
+                    background-color: #008000; /* Emerald Green */
+                    align-self: flex-end; /* This will align the sender's messages to the right */
+                }
+
+                .bot {
+                    background-color: #808080; /* Gray */
+                    align-self: flex-start; /* This will align the bot's responses to the left */
+                }
+
+                
                 .message {
                     margin: 10px;
                     padding: 10px;
                 }
+
+
                 .user {
                     background-color: #d0f0d0;
                 }
@@ -49,6 +109,41 @@ def home():
                         display: flex;
                         } 
 
+                    #upload-button {
+                    color: white;
+                    background-color: #007bff;
+                    border: none;
+                    padding: 5px 10px;
+                    border-radius: 5px;
+                    font-size: 14px;
+                    cursor: pointer;
+                    transition: background-color 0.3s ease;
+                    margin-top: 12px;
+                    }
+
+                    #message-input {
+                    bottom: 0;
+                    width: 100%;
+                    flex-grow: 1;
+                    font-size: 16px;
+                    box-sizing: border-box;
+                    border: none;
+                    padding: 10px 0 10px 12px;
+                    border-radius: 40px 0 0 40px;
+                    background-color: transparent;
+                    height: auto;
+                    }
+
+                    .input-group {
+                    position: relative;
+                    display: flex;
+                    flex-wrap: nowrap;
+                    align-items: stretch;
+                    width: 100%;
+                    border-radius: 40px;
+                    border: 1px solid #2d2d2d;
+                    }
+
                         #reset-button {
                         border: none;
                         font: inherit;
@@ -66,28 +161,58 @@ def home():
            
         </head>
         <body>
+
          
-            <div class="container">
-                <h1 class="text-center">Smart Tourist</h1>
-                <p>Hello there! I'm your Smart Tourist guide. I can answer anything, with up-to-date information.</p>
-                <div class="scrollable">
-                    {% for message in messages %}
-                        <div class="message alert {{ 'alert-success' if message.sender == 'user' else 'alert-primary' }}">
-                            {{ message.text }}
-                        </div>
-                    {% endfor %}
+
+
+            <div class="container-main">
+                <div class="row">
+                <!-- Heading -->
+                <div class="col-12">
+                    <h1 class="display-4">Smart Tourist</h1>
                 </div>
-                <form method="post" class="form-inline justify-content-center">
-                    <input type="text" id="message-input" name="message" class="form-control mb-2 mr-sm-2" placeholder="Type your message here...">
+
+                <!-- Description -->
+                <div class="col-12">
+                    <p class="lead">Hello there! I'm your Smart Tourist guide. I can answer anything, with up-to-date information.</p>
+                </div>
+                </div>
+            </div>
+                
+
+            <div class="container">
+                <div class="row">
+                <div>
+                    <input type="file" id="file-upload" accept=".pdf" hidden name="file[]" multiple="">
+                    <button id="upload-button" class="btn btn-primary btn-sm">Upload File</button>
+                </div>
+                <!-- Chat Element -->
+                <div class="col-12">
+                    <div class="chat-element" style="height: 200px; overflow-y: auto;">
+                        {% for message in messages %}
+                            <div class="message-box sender" style="text-align: right;">
+                                <div class="me">{{ message.sender }}</div>
+                            </div>
+                            <div class="message-box bot" style="text-align: left;">
+                                <div class="message-line">{{ message.text }}</div>
+                            </div>
+                        {% endfor %}
+                    </div>
+                </div>
+            </div>
+
+
                     
+                </div>
+    
+
+                <form method="post" class="form-inline justify-content-center"  >
+                    <input type="text" id="message-input" name="message"  class="form-control mb-2 mr-sm-2" style="width: 80%;" placeholder="Type your message here...">
                     
-                    
-                    <button type="button" onclick="record()" id="speak-button"  class="btn btn-primary send"><i class="fa fa-microphone"></i></button>
-                        </br>
-                        <div>
                     <button  name="send" id="send-button" class="btn btn-primary send">
                             <i class='fa fa-paper-plane'></i>
                         </button>
+                    <button type="button" onclick="record()" id="speak-button"  class="btn btn-primary send"><i class="fa fa-microphone"></i></button>
                     <button name="reset" id="reset-button" class="btn btn-primary reset">
                             <i class="fas fa-sync"></i>
                         </button>
